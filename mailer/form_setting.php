5626def3c26b31c280209dcea7032dce4b135aa6
Blackyout <reshinger@gmail.com>
2022-11-10 13:55:45 -0500

    add cv

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,6 +1,6 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
+	$subj = "New message from the site 'vCard & Resume Site'"; //letter subject
 	$to = 'reshinper@gmail.com'; // Enter Your E-mail
 	$from = 'reshinger@gmail.com'; // Admin e-mail
 	$fromName = 'Your Company Name'; // Your company name
