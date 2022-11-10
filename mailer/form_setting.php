30fa55e482bf8eb694237fcbd6aca4e331b62391
Blackyout <reshinger@gmail.com>
2022-11-10 13:40:38 -0500

    Subiendo portfolio v0

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
+	$to = 'reshinper@gmail.com'; // Enter Your E-mail
+	$from = 'reshinger@gmail.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
